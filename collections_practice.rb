4ff76e3bb62d7b89e6c58b01346c2527ac0d5d29
Brandon Teich <brandonteich123@gmail.com>
2018-11-10 20:12:11 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
 
+def sort_array_asc(intarray) 
+  intarray.sort
+end
+
+def sort_array_desc(intarray) 
+  intarray.sort do |x,y|
+    y<=>x
+  
+  end
+end
+
+def sort_array_char_count(stringarray) 
+  stringarray.sort do |x,y|
+    x<=>y
+    
